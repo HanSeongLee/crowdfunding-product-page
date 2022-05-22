6dc53afdd71a3e274ee9ac85a0648365b3454c79
HanSeong Lee <jkhanseong@gmail.com>
2022-05-22 17:32:17 +0900

    ADD | Menu on mobile

---
+++ components/Menu/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import styles from './style.module.scss';
+import SitemapContainer from '../../containers/SitemapContainer';
+
+interface IProps {
+    open: boolean;
+    toggleMenu: () => void;
+};
+
+const Menu: React.FC<IProps> = ({ open, toggleMenu }) => {
+    return (
+        <nav className={styles.menuNav}>
+            <button className={styles.menuButton}
+                    type={'button'}
+                    onClick={toggleMenu}
+            >
+                <img src={!open ? '/icons/icon-hamburger.svg' : '/icons/icon-close-menu.svg'}
+                     alt={'Menu'}
+                />
+            </button>
+
+            {open && (
+                <div className={styles.menuContainerWrapper}>
+                    <SitemapContainer className={styles.menuContainer}/>
+                </div>
+            )}
+        </nav>
+    );
+};
+
+export default Menu;
