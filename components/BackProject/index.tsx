e9177a69136025e26996510050453707f366861b
HanSeong Lee <jkhanseong@gmail.com>
2022-05-28 17:14:49 +0900

    ADD | BackProject on mobile

---
+++ components/BackProject/index.tsx
@@ -0,0 +1,39 @@
+import React, {HTMLAttributes} from 'react';
+import styles from './style.module.scss';
+import CloseIcon from '/public/icons/icon-close-menu.svg';
+import RewardOptionContainer from '../../containers/RewardOptionContainer';
+import {IReward} from '../../types/reward';
+
+interface IProps extends HTMLAttributes<HTMLDivElement> {
+    onContinue: (reward: IReward, pledge?: number) => void;
+    onClose: () => void;
+};
+
+const BackProject: React.FC<IProps> = ({ onContinue, onClose, ...props }) => {
+    return (
+        <div className={styles.backProject}
+             {...props}
+        >
+            <div className={styles.titleWrapper}>
+                <h2 className={styles.title}>
+                    Back this project
+                </h2>
+                <button className={styles.closeButton}
+                        aria-label={'close'}
+                        onClick={onClose}
+                >
+                    <CloseIcon/>
+                </button>
+            </div>
+            <p className={styles.description}>
+                Want to support us in bringing Mastercraft Bamboo Monitor Riser out in the world?
+            </p>
+
+            <RewardOptionContainer className={styles.rewardOptionContainer}
+                                   onContinue={onContinue}
+            />
+        </div>
+    );
+};
+
+export default BackProject;
